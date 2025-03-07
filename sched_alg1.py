ba20f41db2c57b01d8256a35c7e17676b5fe6f91
therealnima <117394462+therealnima@users.noreply.github.com>
2025-03-07 12:56:55 -0600

    PRIORITY COMPLETED
    Used a min-heap for the priority so that the lowest priority is at the top of the heap.

--- sched_alg1.py
+++ sched_alg1.py
@@ -3,6 +3,7 @@
 #THE NAME OF THE ALGORITHM GOES HERE
 #Priority
 #CLASSES GO HERE
+import heapq
 class Process:
     def __init__(self,priority,burst_time):
         self.priority= priority
@@ -15,32 +16,48 @@ class Process:
         if not isinstance(other,Process):
             return NotImplemented
         return self.priority < other.priority 
-
-
+    
 class CustomQueue:
     def __init__(self):
         self.queue=[]
 
     def put(self,process):
-        self.queue.append(process)
+        heapq.heappush(self.queue,process)
 
     def get(self):
-        if len(self.queue)>=1:
-            return self.queue.pop(0)
-        return None
-    def __lt__(self,other):
-        pass
+        if self.queue:
+            return heapq.heappop(self.queue)
+        else:
+            return None
+        
+    def length(self):
+        return len(self.queue)
         
 
 
 class CustomScheduler:
-    def schedule(self):
-        pass
+    def __init__(self):
+        self.process_queue= CustomQueue()
+
+    def schedule(self,process_list):
+            for process in process_list:
+                self.process_queue.put(process)
 
     def consume(self):
-        pass
+        while self.process_queue.length() > 0:
+            process = self.process_queue.get()
+            print(f"Executing: {process}")
 
 if __name__ == '__main__':
 #REMOVE PASS AND DEMONSTRATE THE SCHEDULER IN ACTION HERE
 #CREATE ANY QUEUES, PROCESSES, CALL APPROPRIATE METHODS
-    pass 
+    processes=[
+        Process(4,10),
+        Process(2,5),
+        Process(1,5),
+        Process(5,3)
+    ]
+    scheduler=CustomScheduler()
+    scheduler.schedule(processes)
+    scheduler.consume()
+
